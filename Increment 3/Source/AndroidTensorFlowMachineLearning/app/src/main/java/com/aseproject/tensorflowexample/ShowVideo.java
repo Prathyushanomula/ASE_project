c1feaf925a35e4fcb375d301245bd56b4d29e40e
sravyapara <35548441+sravyapara@users.noreply.github.com>
2018-04-25 10:44:40 -0500

    commit

---
+++ Increment 3/Source/AndroidTensorFlowMachineLearning/app/src/main/java/com/aseproject/tensorflowexample/ShowVideo.java
@@ -0,0 +1,81 @@
+package com.aseproject.tensorflowexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class ShowVideo
+        extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 10;
+    public static final String API_KEY = DeveloperKey.DEVELOPER_KEY;
+
+    //From URL -> https://www.youtube.com/watch?v=kHue-HaXXzg
+    // Let It Go : "Frozen"
+    private String VIDEO_ID = "8EFSLb91mb8";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_video);
+
+        Intent i = getIntent();
+        VIDEO_ID = i.getStringExtra("videoid");
+
+                YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format("YouTube Error (%1$s)",
+                    errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(API_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+    /*
+              try {
+                  Intent i = getIntent();
+                  String videoid = "ZGL0HpNm5BY";//i.getStringExtra("videoid");
+                  player.loadVideo(videoid);
+              }
+              catch(Exception exp){
+                  System.out.println("err3 "+exp);
+              }
+              */
+
+}
